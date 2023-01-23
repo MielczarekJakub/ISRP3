9d1d0f0e75b5abbcb30beac173cddbdd7248f312
Wiktor Malavasi <74613486+MisterMal@users.noreply.github.com>
2023-01-23 16:32:26 +0100

    CreateLocationTest fix

--- src/test/java/CreateLocationTest.java
+++ src/test/java/CreateLocationTest.java
@@ -1,17 +1,17 @@
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 
 import java.time.Duration;
 
 
-public class CreateLocationTest extends BaseTest {
+public class CreateKicatuinTest extends BaseTest {
 
     // LOCATION PATTERN XX-00-00-00
     protected String locationName = "-01-02-03";
@@ -20,13 +20,13 @@ public class CreateLocationTest extends BaseTest {
 
 
     @ParameterizedTest
-    @ValueSource(classes = {ChromeDriver.class, EdgeDriver.class})
+    @ValueSource(classes = {ChromeDriver.class, FirefoxDriver.class})
     public void basicTest(Class<? extends WebDriver> webDriverClass) {
         if (webDriverClass.equals(ChromeDriver.class)) {
             driver = new ChromeDriver(chromeOptions);
             locationPattern = "CH" + locationName;
         } else {
-            driver = new EdgeDriver(edgeOptions);
+            driver = new FirefoxDriver(firefoxOptions);
             locationPattern = "ED" + locationName;
         }
 
@@ -34,33 +34,33 @@ public class CreateLocationTest extends BaseTest {
         driver.get(baseUrl + "common/signIn.xhtml");
         // Log in
         driver.findElement(By.cssSelector("input[name='j_username']")).sendKeys(username1);
-        driver.findElement(By.cssSelector("input[name='j_password']")).sendKeys(password1);
+        driver.findElement(By.cssSelector("input[name='j_password']")).sendKeys(basicPassword);
         driver.findElement(By.cssSelector("input[value='Zaloguj']")).click();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-        // navigate to create location page
+        // zmiana położenia do tworzenia lokalizacji
         driver.findElement(By.xpath("//a[contains(normalize-space(), 'Lokalizacja')]")).click();
         driver.findElement(By.xpath("//a[contains(normalize-space(), 'Utwórz nową lokalizację')]")).click();
 
-        // create location
+        // stworzenie lokalizacji
         driver.findElement(By.cssSelector("input[id='CreateLocationForm:locationSymbol']")).sendKeys(locationPattern);
         WebElement selectElement = driver.findElement(By.cssSelector("select[id='CreateLocationForm:locationType']"));
         Select select = new Select(selectElement);
         select.selectByVisibleText(locationType);
         driver.findElement(By.cssSelector("input[name='CreateLocationForm:j_idt34']")).click();
 
-        // navigate to location list
+        // zmiana położenia na listę lokalizacji
         driver.findElement(By.cssSelector("a[class='dropdown-toggle']"))
                 .findElement(By.xpath("//a[contains(normalize-space(), 'Lokalizacja')]")).click();
         driver.findElement(By.xpath("//a[contains(normalize-space(), 'Lista lokalizacji')]")).click();
 
 
-        // check if location is on the list
+        // czy lokacja na liscie
         WebElement element = driver.findElement(By.xpath("//td[contains(normalize-space(), "  + locationPattern + ")]"));
         Assertions.assertNotNull(element);
 
-        // logout
+        // wylogowanie
         driver.findElement(By.xpath("//a[contains(normalize-space(), 'Wylogowanie')]")).click();
     }
 }

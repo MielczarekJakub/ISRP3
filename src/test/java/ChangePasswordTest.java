98df043686e3542d3de41e86516d34e76fef744d
Wiktor Malavasi <74613486+MisterMal@users.noreply.github.com>
2023-01-23 16:30:45 +0100

    ChangePasswordTest fix

--- src/test/java/ChangePasswordTest.java
+++ src/test/java/ChangePasswordTest.java
@@ -5,48 +5,47 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
-public class ChangePasswordTest extends BaseTest{
-
-    protected String newPassword = "H@selk0!";
+public class ChangePasswordTest extends BaseTest {
+    protected String newPassword = "noweH@s!0";
 
     @ParameterizedTest
-    @ValueSource(classes={ChromeDriver.class, EdgeDriver.class})
+    @ValueSource(classes={ChromeDriver.class, FirefoxDriver.class})
     public void basicTest(Class<? extends WebDriver> webDriverClass){
         if (webDriverClass.equals(ChromeDriver.class)) {
             driver = new ChromeDriver(chromeOptions);
         } else {
-            driver = new EdgeDriver(edgeOptions);
+            driver = new FirefoxDriver(firefoxOptions);
         }
 
         driver.get(baseUrl + "common/signIn.xhtml");
         // Log in
         driver.findElement(By.cssSelector("input[name='j_username']")).sendKeys(username1);
-        driver.findElement(By.cssSelector("input[name='j_password']")).sendKeys(password1);
+        driver.findElement(By.cssSelector("input[name='j_password']")).sendKeys(basicPassword);
 
         driver.findElement(By.cssSelector("input[value='Zaloguj']")).click();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
 
-        //navigate to change password page
+        //zmiana ścieżki na formularz zmiany hasła
         driver.findElement(By.xpath("//a[contains(normalize-space(), 'Ustawienia')]")).click();
         driver.findElement(By.xpath("//a[contains(normalize-space(), 'Zmiana hasła')]")).click();
 
-        //changing password
-        driver.findElement(By.cssSelector("input[id='ChangeMyPasswordForm:oldPassword']")).sendKeys(password1);
+        //podanie danych do zmiany hasla oraz zatwierdzenie zmiany
+        driver.findElement(By.cssSelector("input[id='ChangeMyPasswordForm:oldPassword']")).sendKeys(basicPassword);
         driver.findElement(By.cssSelector("input[id='ChangeMyPasswordForm:newPassword']")).sendKeys(newPassword);
         driver.findElement(By.cssSelector("input[id='ChangeMyPasswordForm:newPasswordRepeat']")).sendKeys(newPassword);
         driver.findElement(By.cssSelector("input[value='Zmień hasło']")).click();
 
-        //log out
+        //wylogowanie
         driver.findElement(By.xpath("//a[contains(normalize-space(), 'Wylogowanie')]")).click();
         driver.findElement(By.cssSelector("input[value='Wyloguj']")).click();
 
-        //second login attempt with changed data
+        //próba logowania po zmianie hasła
         driver.get(baseUrl + "common/signIn.xhtml");
         driver.findElement(By.cssSelector("input[name='j_username']")).sendKeys(username1);
         driver.findElement(By.cssSelector("input[name='j_password']")).sendKeys(newPassword);
@@ -58,13 +57,13 @@ public class ChangePasswordTest extends BaseTest{
         WebElement element = driver.findElement(By.xpath("//h4[contains(normalize-space(), 'JDoe')]"));
         Assertions.assertNotNull(element);
 
-        //restore original state
+        //cofnięcie zmiany hasla
         driver.findElement(By.xpath("//a[contains(normalize-space(), 'Ustawienia')]")).click();
         driver.findElement(By.xpath("//a[contains(normalize-space(), 'Zmiana hasła')]")).click();
 
         driver.findElement(By.cssSelector("input[id='ChangeMyPasswordForm:oldPassword']")).sendKeys(newPassword);
-        driver.findElement(By.cssSelector("input[id='ChangeMyPasswordForm:newPassword']")).sendKeys(password1);
-        driver.findElement(By.cssSelector("input[id='ChangeMyPasswordForm:newPasswordRepeat']")).sendKeys(password1);
+        driver.findElement(By.cssSelector("input[id='ChangeMyPasswordForm:newPassword']")).sendKeys(basicPassword);
+        driver.findElement(By.cssSelector("input[id='ChangeMyPasswordForm:newPasswordRepeat']")).sendKeys(basicPassword);
         driver.findElement(By.cssSelector("input[value='Zmień hasło']")).click();
 
         driver.findElement(By.xpath("//a[contains(normalize-space(), 'Wylogowanie')]")).click();

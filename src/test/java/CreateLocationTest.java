d2a28b7179d8af795ea58e85482eab80e7c7e08d
Wiktor Malavasi <74613486+MisterMal@users.noreply.github.com>
2023-01-24 19:28:04 +0100

    Update CreateLocationTest.java

--- src/test/java/CreateLocationTest.java
+++ src/test/java/CreateLocationTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.By;
+import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
